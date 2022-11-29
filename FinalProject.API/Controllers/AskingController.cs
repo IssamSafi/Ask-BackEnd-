3c99c09a60c275a5c1d20d4040f624cad75bab0f
IssamSafi <90898382+IssamSafi@users.noreply.github.com>
2022-11-29 21:27:36 +0200

    accept3true

--- FinalProject.API/Controllers/AskingController.cs
+++ FinalProject.API/Controllers/AskingController.cs
@@ -13,9 +13,64 @@ namespace FinalProject.API.Controllers
     [ApiController]
     public class AskingController : Controller<Asking>
     {
+
+        private readonly IService<Asking> _service;
         public AskingController(IService<Asking> service) : base(service)
         {
+            _service = service;
+        }
 
+        [HttpGet]
+        public ActionResult GetAllAskingUsers()
+        {
+            try
+            {
+                var list = _service.GetAll();
+                return Ok(list);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        [HttpPost]
+        public ActionResult TakeActionStatus(int AskId, string status)
+        {
+            try
+            {
+                var AskObj = _service.GetById(AskId);
+                if (AskObj != null)
+                {
+                    AskObj.Itsapprove = Convert.ToDecimal(status);
+                    _service.Update(AskObj);
+
+                    ///send email
+
+
+                    return Ok(true);
+                }
+
+                return Ok(false);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        [HttpGet]
+        public ActionResult GetAsksByUserID(int UserId)
+        {
+            try
+            {
+                var list = _service.GetAll().Where(x => x.UserId == UserId);
+                return Ok(list);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
 
     }
