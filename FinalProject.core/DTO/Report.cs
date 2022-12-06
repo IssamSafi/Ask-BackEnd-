e0865b601b9e36892b8858dbb40adcc600f2d515
Abdullah Watheq Albarazi <abdallahwatheqalbarazi@gmail.com>
2022-12-06 16:11:58 +0300

    allquestion

--- FinalProject.core/DTO/Report.cs
+++ FinalProject.core/DTO/Report.cs
@@ -9,6 +9,7 @@ namespace FinalProject.core.DTO
         public string Fname { get; set; }
         public string Lname { get; set; }
         public decimal? Itsapprove { get; set; }
+        public string Image_Path { get; set; }
         public DateTime? Askingdate { get; set; }
         public string Messege { get; set; }
     }
